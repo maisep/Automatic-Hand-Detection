e48a4a0e0ecd614abe9a4c1551c8fe1b62e3d1b4
maisep <chaser96144@gmail.com>
2023-05-17 12:25:56 +0900

    Update hand_detect.py
    upgrade to YOLOv8, fix json

--- hand_detect.py
+++ hand_detect.py
@@ -1,9 +1,22 @@
+from typing import Any
 import cv2
 import numpy as np
 import mediapipe as mp
 import json
 import os
 from pathlib import Path
+from ultralytics import YOLO
+
+class MyJSONEncoder(json.JSONEncoder):
+    def default(self, o: Any) -> Any:
+        if isinstance(o, np.integer):
+            return int(o)
+        elif isinstance(o, np.floating):
+            return float(o)
+        elif isinstance(o, np.ndarray):
+            return o.tolist()
+        else:
+            return super(MyJSONEncoder, self).default(o)
 
 class HandDetection:
     def __init__(self, file_path, save_path, image, file, draw_person_rect: bool = False, draw_hand_rect: bool = False):
@@ -38,38 +51,55 @@ class HandDetection:
 
     # Yolo로 물체 찾기
     def __find_object(self):
-        # Load Yolo
-        net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
-        
-        with open("coco.names", "r") as f:
-            self.classes = [line.strip() for line in f.readlines()]
-        layer_names = net.getLayerNames()
-        output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
-        self.colors = np.random.uniform(0, 255, size=(len(self.classes), 3))
+        # Load YOLO
+        model = YOLO('yolov8x.pt')
 
         self.height, self.width, self.channels = self.image.shape
 
-        blob = cv2.dnn.blobFromImage(self.image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-        net.setInput(blob)
-        outs = net.forward(output_layers)
+        results = model.predict(source=self.image)
+        boxes = results[0].boxes.xyxy.cpu().numpy().astype(int)
+        classes = results[0].boxes.cls.cpu().numpy().astype(int)
+        confs = results[0].boxes.conf.cpu().numpy().astype(float)
 
-        for out in outs:
-            for detection in out:
-                scores = detection[5:]
-                class_id = np.argmax(scores)
-                confidence = scores[class_id]
-                if confidence > 0.5:
-                    # Object detected
-                    center_x = int(detection[0] * self.width)
-                    center_y = int(detection[1] * self.height)
-                    w = int(detection[2] * self.width)
-                    h = int(detection[3] * self.height)
-                    # Rectangle coordinates
-                    x = int(center_x - w / 2)
-                    y = int(center_y - h / 2)
-                    self.boxes.append([x, y, w, h])
-                    self.confidences.append(float(confidence))
-                    self.class_ids.append(class_id)
+        for box, cls, conf in zip(boxes, classes, confs):
+            if cls == 0 and conf > 0.5:
+                # center_x, center_y, w, h = box
+                # self.boxes.append(center_x, center_y, w, h)
+                self.boxes.append([box[0], box[1], box[2] - box[0], box[3] - box[1]])
+                self.confidences.append(conf)
+
+        # # Load Yolo
+        # net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+        
+        # with open("coco.names", "r") as f:
+        #     self.classes = [line.strip() for line in f.readlines()]
+        # layer_names = net.getLayerNames()
+        # output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+        # self.colors = np.random.uniform(0, 255, size=(len(self.classes), 3))
+
+        # self.height, self.width, self.channels = self.image.shape
+
+        # blob = cv2.dnn.blobFromImage(self.image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+        # net.setInput(blob)
+        # outs = net.forward(output_layers)
+
+        # for out in outs:
+        #     for detection in out:
+        #         scores = detection[5:]
+        #         class_id = np.argmax(scores)
+        #         confidence = scores[class_id]
+        #         if confidence > 0.5:
+        #             # Object detected
+        #             center_x = int(detection[0] * self.width)
+        #             center_y = int(detection[1] * self.height)
+        #             w = int(detection[2] * self.width)
+        #             h = int(detection[3] * self.height)
+        #             # Rectangle coordinates
+        #             x = int(center_x - w / 2)
+        #             y = int(center_y - h / 2)
+        #             self.boxes.append([x, y, w, h])
+        #             self.confidences.append(float(confidence))
+        #             self.class_ids.append(class_id)
 
         self.__find_person(self.draw_person_rect)
 
@@ -80,29 +110,31 @@ class HandDetection:
         for i in range(len(self.boxes)):
             if i in indexes:
                 x, y, w, h = self.boxes[i]
-                label = str(self.classes[self.class_ids[i]])
-                if label == "person":
-                    if draw_rect:
-                        color = self.colors[i]
-                        cv2.rectangle(self.image, (x, y), (x + w, y + h), color, 2)
-                        cv2.putText(self.image, label, (x, y + 30), font, 3, color, 3)
+                # label = str(self.classes[self.class_ids[i]])
+            # if label == "person":
+                if draw_rect:
+                    # color = self.colors[i]
+                    color = (0, 0, 255)
+                    cv2.rectangle(self.image, (x, y), (x + w, y + h), color, 2)
+                    # cv2.putText(self.image, label, (x, y + 30), font, 3, color, 3)
+                    cv2.putText(self.image, "person", (x, y + 30), font, 3, color, 3)
 
-                    self.person_pos.append([x, y, x + w, y + h])
+                self.person_pos.append([x, y, x + w, y + h])
 
-                    ny = y - 50
-                    nx = x - 50
-                    y_1 = ny if ny >= 0 else 0
-                    x_1 = nx if nx >= 0 else 0
+                ny = y - 50
+                nx = x - 50
+                y_1 = ny if ny >= 0 else 0
+                x_1 = nx if nx >= 0 else 0
 
-                    ny = y + h + 50
-                    nx = x + w + 50
-                    y_2 = ny if ny <= self.height else self.height
-                    x_2 = nx if nx <= self.width else self.width
+                ny = y + h + 50
+                nx = x + w + 50
+                y_2 = ny if ny <= self.height else self.height
+                x_2 = nx if nx <= self.width else self.width
 
-                    tmp_image = self.image[y_1:y_2, x_1:x_2]
-                    if len(tmp_image) == 0:
-                        continue
-                    self.__find_hand(tmp_image, [x_1, y_1, x_2, y_2], self.draw_hand_rect)
+                tmp_image = self.image[y_1:y_2, x_1:x_2]
+                if len(tmp_image) == 0:
+                    continue
+                self.__find_hand(tmp_image, [x_1, y_1, x_2, y_2], self.draw_hand_rect)
 
         self.__save_result()
 
@@ -111,7 +143,7 @@ class HandDetection:
         with self.mp_hands.Hands(
             static_image_mode=True,
             max_num_hands=10,
-            min_detection_confidence=0.4
+            min_detection_confidence=0.35
             ) as hands:
             results = hands.process(cv2.cvtColor(person_image, cv2.COLOR_BGR2RGB))
             
@@ -202,15 +234,15 @@ class HandDetection:
         if not exists:
             output_dict.append(output_image_dict)
         
-        Path(self.save_path + '/' + jsonfile_name).write_text(json.dumps(output_dict))
-
         cv2.imwrite(self.save_path + '/' + self.file, self.image)
+        Path(self.save_path + '/' + jsonfile_name).write_text(json.dumps(output_dict, cls=MyJSONEncoder))
+
 
 def main(file_path, save_path):
     file_list = os.listdir(file_path)
     for file in file_list:
         image = cv2.imread(file_path + '/' + file)
-        HandDetection(file_path, save_path, image, file)
+        HandDetection(file_path, save_path, image, file, True, True)
 
 if __name__ == '__main__':
     file_path = './origin'
